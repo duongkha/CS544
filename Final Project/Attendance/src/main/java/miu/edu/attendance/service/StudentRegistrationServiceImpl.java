99e55d707b4c77d76e9f21f3defea5d4a1aa8463
yohannnes kelati <johnkelati16@gmail.com>
2021-06-06 23:03:58 -0500

    Student registration is complete

---
+++ Final Project/Attendance/src/main/java/miu/edu/attendance/service/StudentRegistrationServiceImpl.java
@@ -0,0 +1,52 @@
+package miu.edu.attendance.service;
+
+import miu.edu.attendance.domain.CourseOffering;
+import miu.edu.attendance.domain.Student;
+import miu.edu.attendance.dto.StudentRegistrationDTO;
+import miu.edu.attendance.repository.CourseOfferingRepository;
+import miu.edu.attendance.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class StudentRegistrationServiceImpl implements StudentRegistrationService {
+
+@Autowired
+StudentService studentService;
+@Autowired
+StudentRepository studentRepository;
+@Autowired
+CourseOfferingService courseOfferingService;
+@Autowired
+CourseOfferingRepository courseOfferingRepository;
+
+
+    @Override
+    public boolean register(StudentRegistrationDTO studentRegistrationDTO) {
+
+        Student student= studentRepository.findAll().
+                stream().filter(student1->student1.getStudentId().equalsIgnoreCase(studentRegistrationDTO.getStudentId()))
+                .collect(Collectors.toList()).get(0);
+        if(student!=null){
+            CourseOffering courseOffering= courseOfferingRepository.findAll().
+                    stream().filter(course->course.getCourse().getCourseCode().equalsIgnoreCase(studentRegistrationDTO.getCourseCode()))
+                    .collect(Collectors.toList()).get(0);
+
+            if(courseOffering!=null){
+                student.addCourseOfferings(courseOffering);
+                studentRepository.save(student);
+                courseOfferingRepository.save(courseOffering);
+                return true;
+            }
+            return false;
+
+        }
+
+        return false;
+
+
+
+    }
+}
