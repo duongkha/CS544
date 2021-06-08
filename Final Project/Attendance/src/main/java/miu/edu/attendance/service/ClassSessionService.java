b49ff015f55f41105d1f63de42ff669ade6d299b
yohannnes kelati <johnkelati16@gmail.com>
2021-06-07 22:22:22 -0500

    Class session completed

--- Final Project/Attendance/src/main/java/miu/edu/attendance/service/ClassSessionService.java
+++ Final Project/Attendance/src/main/java/miu/edu/attendance/service/ClassSessionService.java
@@ -1,18 +1,17 @@
 package miu.edu.attendance.service;
 
 import miu.edu.attendance.domain.ClassSession;
-import miu.edu.attendance.domain.TimeSlot;
-import miu.edu.attendance.dto.ClassSessionDTO;
-import miu.edu.attendance.dto.TimeSlotDTO;
+import miu.edu.attendance.dto.ClassSessionRequestDTO;
+import miu.edu.attendance.dto.ClassSessionResponseDTO;
 
 import java.util.List;
 
 public interface ClassSessionService {
 
-    public boolean addClassSession(ClassSessionDTO classSessionDTO);
-    public boolean updateClassSession(ClassSessionDTO classSessionDTO);
+    public boolean addClassSession(ClassSessionRequestDTO classSessionRequestDTO);
+    public boolean updateClassSession(ClassSessionRequestDTO classSessionRequestDTO);
     public boolean deleteClassSession(Long id);
-    public ClassSession findTClassSessionByID(Long id);
-    public List<ClassSession> getAllClassSessions();
+    public ClassSessionResponseDTO findTClassSessionByID(Long id);
+    public List<ClassSessionResponseDTO> getAllClassSessions();
 
 }
